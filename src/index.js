eb78fec27baae50d9d2a80f11a157a662715bf8b
Dfelix02 <david.felix02@hotmail.com>
2020-09-18 13:30:18 -0400

    second deliverable

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ let addToy = false;
 
 let toyCollection = document.querySelector("#toy-collection");
 
+let newToyName = document.querySelector(".add-toy-form")
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -16,6 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
   });
+
   fetch("http://localhost:3000/toys")
     .then(res => res.json())
     .then(toyname => createNewToy(toyname))
@@ -40,20 +42,49 @@ document.addEventListener("DOMContentLoaded", () => {
       toyDiv.append(toyButton)
       toyCollection.append(toyDiv)
      });
-      // let toyDiv = document.createElement("div")
-      // toyDiv.className = "card"
-      // let toyH2 = document.createElement("h2")
-      // toyH2.innerText = toy.name
-      // toyDiv.append(toyH2)
-      // let toyImg = document.createElement("img")
-      // toyImg.src= toy.image
-      // toyDiv.append(toyImg)
-      // let toyP = document.createElement("p")
-      // toyP.innerText = toy.likes
-      // toyDiv.append(toyP)
-      // let toyButton = document.createElement("button")
-      // toyButton.innerText = "Like"
-      // toyDiv.appened(toyButton)
     }
+    newToyName.addEventListener("submit", function(evt) {
+      evt.preventDefault()
+      const newToyName = evt.target.name.value
+      const newToyUrl = evt.target.image.value
+      fetch("http://localhost:3000/toys", {
+        method: "POST",
+        headers: 
+        {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({
+          "name": newToyName,
+          "image": newToyUrl,
+          "likes": 0
+        })
+      }) 
+      .then(res => res.json())
+      .then(newToyName => createNewToyName(newToyName))
+
+      let createNewToyName = (newToyName) => {
+
+        let toyDiv = document.createElement("div")
+        toyDiv.className = "card"
+        let toyH2 = document.createElement("h2")
+        toyH2.innerText = newToyName.name
+        toyDiv.append(toyH2)
+        let toyImg = document.createElement("img")
+        toyImg.src= newToyName.image
+        toyImg.className = "toy-avatar"
+        toyDiv.append(toyImg)
+        let toyP = document.createElement("p")
+        toyP.innerText = `${newToyName.likes} likes` 
+        toyDiv.append(toyP)
+        let toyButton = document.createElement("button")
+        toyButton.innerText = "Like"
+        toyButton.className = "link-btn"
+        toyDiv.append(toyButton)
+        toyCollection.append(toyDiv)
+      }
+    })
+    
+
 })
 
